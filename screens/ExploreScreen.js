2a043bca26fbf6f8321768f368e6e9715839fda4
Wenqian Xie <wenqianxie@Wenqians-MacBook-Air.local>
2023-11-19 16:55:51 -0800

    Add before icon and let all user selections belong to one branch

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -1,50 +1,54 @@
 import React from "react";
-import { View, Text, ImageBackground} from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 import { styles } from "../helper/HelperStyles";
 import DialogButton from "../components/DialogButton";
 import { colors } from "../helper/HelperColors";
 import ArrowButton from "../components/ArrowButton";
 
-export function ExploreScreen({navigation}) {
+export function ExploreScreen({ navigation }) {
   const arrowButtonHandler = () => {
-    navigation.navigate("MustDo");
-  }  
+    navigation.navigate("MustDo", { questionType: "lengthInCanada" });
+  };
   const essentialButtonHandler = () => {
     navigation.navigate("Essential");
-  }  
+  };
   const medicineButtonHandler = () => {
     navigation.navigate("Medicine");
-  }
+  };
   const transitButtonHandler = () => {
     navigation.navigate("Transit");
-  }
+  };
   return (
     <View style={styles.container}>
-      <ImageBackground source={require("../assets/explore_background.png")} style={styles.backgroundPic}>
-
+      <ImageBackground
+        source={require("../assets/explore_background.png")}
+        style={styles.backgroundPic}
+      >
         <Text style={styles.text}>Tap Your Desired Topic to Start! </Text>
         <ArrowButton
           onPress={arrowButtonHandler}
           text={"Lost?\nBegin Here!"}
-          position={styles.arrowButtonPosition}/>
+          position={styles.arrowButtonPosition}
+        />
 
-        <DialogButton 
-          onPress={essentialButtonHandler} 
-          text="Obtain Essential Docs" 
-          position={styles.essentialButtonPosition}/>
+        <DialogButton
+          onPress={essentialButtonHandler}
+          text="Obtain Essential Docs"
+          position={styles.essentialButtonPosition}
+        />
 
-        <DialogButton 
-          onPress={medicineButtonHandler} 
-          text='See Doctor?Get Meds?'
-          position={styles.medicineButtonPosition}/>
+        <DialogButton
+          onPress={medicineButtonHandler}
+          text="See Doctor?Get Meds?"
+          position={styles.medicineButtonPosition}
+        />
 
-        <DialogButton 
-          onPress={transitButtonHandler} 
-          text='Use Public Transit'
-          position={styles.transitButtonPosition}/>
+        <DialogButton
+          onPress={transitButtonHandler}
+          text="Use Public Transit"
+          position={styles.transitButtonPosition}
+        />
       </ImageBackground>
     </View>
   );
 }
-
-
--- screens/MustDoQuestionnaire.js
+++ screens/MustDoQuestionnaire.js
@@ -4,47 +4,112 @@ import Checkbox from "expo-checkbox";
 import { writeToUsersDB } from "../firebase/FirebaseHelper";
 import { MaterialIcons } from "@expo/vector-icons";
 
-const MustDoQuestionnaire = () => {
+const MustDoQuestionnaire = ({ navigation, route }) => {
+  const { questionType } = route.params;
+
   const [lengthInCanada, setLengthInCanada] = useState(null);
+  const [occupation, setOccupation] = useState(null);
 
   const handleCheckboxChange = (option) => {
-    setLengthInCanada(option);
+    if (questionType === "lengthInCanada") {
+      setLengthInCanada(option);
+    } else if (questionType === "occupation") {
+      setOccupation(option);
+    }
   };
 
   const handleSubmit = async () => {
-    await writeToUsersDB({ userSelection: lengthInCanada });
+    if (questionType === "lengthInCanada") {
+      navigation.navigate("MustDo", {
+        questionType: "occupation",
+      });
+    } else {
+      await writeToUsersDB({ userSelection: { lengthInCanada, occupation } });
+    }
+  };
+
+  const handlePrevious = () => {
+    navigation.navigate("MustDo", {
+      questionType: "lengthInCanada",
+    });
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.question}>
-        How long have you been arrived in Canada?
-      </Text>
+      {questionType === "lengthInCanada" && (
+        <>
+          <Text style={styles.question}>How long have you been in Canada?</Text>
+          <View style={styles.optionContainer}>
+            <Checkbox
+              value={lengthInCanada === "planningToMove"}
+              onValueChange={() => handleCheckboxChange("planningToMove")}
+            />
+            <Text
+              style={styles.optionText}
+              onPress={() => handleCheckboxChange("planningToMove")}
+            >
+              I am planning to move to Canada
+            </Text>
+          </View>
+          <View style={styles.optionContainer}>
+            <Checkbox
+              value={lengthInCanada === "JustArrived"}
+              onValueChange={() => handleCheckboxChange("JustArrived")}
+            />
+            <Text
+              style={styles.optionText}
+              onPress={() => handleCheckboxChange("JustArrived")}
+            >
+              Just arrived - 6 months
+            </Text>
+          </View>
+        </>
+      )}
+      {questionType === "occupation" && (
+        <>
+          <Text style={styles.question}>What is your occupation?</Text>
 
-      <View style={styles.optionContainer}>
-        <Checkbox
-          value={lengthInCanada === "planningToMove"}
-          onValueChange={() => handleCheckboxChange("planningToMove")}
-        />
-        <Text
-          style={styles.optionText}
-          onPress={() => handleCheckboxChange("planningToMove")}
-        >
-          I am planning to move to Canada
-        </Text>
-      </View>
-      <View style={styles.optionContainer}>
-        <Checkbox
-          value={lengthInCanada === "JustArrived"}
-          onValueChange={() => handleCheckboxChange("JustArrived")}
-        />
-        <Text
-          style={styles.optionText}
-          onPress={() => handleCheckboxChange("JustArrived")}
-        >
-          Just arrived - 6 months
-        </Text>
-      </View>
+          <View style={styles.optionContainer}>
+            <Checkbox
+              value={occupation === "Student"}
+              onValueChange={() => handleCheckboxChange("Student")}
+            />
+            <Text
+              style={styles.optionText}
+              onPress={() => handleCheckboxChange("Student")}
+            >
+              Student
+            </Text>
+          </View>
+
+          <View style={styles.optionContainer}>
+            <Checkbox
+              value={occupation === "Worker"}
+              onValueChange={() => handleCheckboxChange("Worker")}
+            />
+            <Text
+              style={styles.optionText}
+              onPress={() => handleCheckboxChange("Worker")}
+            >
+              Worker
+            </Text>
+          </View>
+
+          <View style={styles.optionContainer}>
+            <Checkbox
+              value={occupation === "Traveller"}
+              onValueChange={() => handleCheckboxChange("Traveller")}
+            />
+            <Text
+              style={styles.optionText}
+              onPress={() => handleCheckboxChange("Traveller")}
+            >
+              Traveller
+            </Text>
+          </View>
+        </>
+      )}
+
       <MaterialIcons
         name="navigate-next"
         size={24}
@@ -52,6 +117,16 @@ const MustDoQuestionnaire = () => {
         onPress={handleSubmit}
         style={styles.submitIcon}
       />
+
+      {questionType === "occupation" && (
+        <MaterialIcons
+          name="navigate-before"
+          size={24}
+          color="black"
+          onPress={handlePrevious}
+          style={styles.previousIcon}
+        />
+      )}
     </View>
   );
 };
