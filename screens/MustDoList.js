7da69d5b680c210c53a6b3a5b951938fd0d309d5
WenqianXie <133101370+WenqianXie@users.noreply.github.com>
2023-12-13 23:07:22 -0800

    Adjusted styles for must-do-list

--- screens/MustDoList.js
+++ screens/MustDoList.js
@@ -1,4 +1,12 @@
-import { View, Text, StyleSheet, Alert, Image, Modal } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  Modal,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { onSnapshot, doc } from "firebase/firestore";
 import { auth } from "../firebase/FirebaseSetup";
@@ -10,19 +18,24 @@ import EntryButtonTextHelper from "../helper/EntryButtonTextHelper";
 import { onAuthStateChanged } from "firebase/auth";
 import IconButton from "../components/IconButton";
 import ReminderSetter from "../components/ReminderSetter";
+import { authStyles } from "../helper/HelperStyles";
 
 export default function MustDoList({ navigation, route }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [generatedMustDoList, setGeneratedMustDoList] = useState([]); 
-  const [bookmarkList, setBookmarkList] = useState([]); 
+  const [generatedMustDoList, setGeneratedMustDoList] = useState([]);
+  const [bookmarkList, setBookmarkList] = useState([]);
   const [randomImageUrl, setRandomImageUrl] = useState("");
-  const [modalVisible, setModalVisible] = useState(false); 
-  const [reminderInfo, setReminderInfo] = useState({title: "", body: "", data: null});
-  const [reminderDateTime, setReminderDateTime] = useState(); 
+  const [modalVisible, setModalVisible] = useState(false);
+  const [reminderInfo, setReminderInfo] = useState({
+    title: "",
+    body: "",
+    data: null,
+  });
+  const [reminderDateTime, setReminderDateTime] = useState();
 
   const passDateTime = (currentDate) => {
-    setDateTime(currentDate)
-  }
+    setDateTime(currentDate);
+  };
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -43,55 +56,64 @@ export default function MustDoList({ navigation, route }) {
 
       const unsubscribe = onSnapshot(bookmarkDocRef, (docSnapshot) => {
         if (docSnapshot.exists()) {
-          console.log("Received generatedMustDoList from database: ", docSnapshot.data().generatedMustDoList);
-          setGeneratedMustDoList(docSnapshot.data().generatedMustDoList); 
-          setBookmarkList(docSnapshot.data().bookmarkList); 
+          console.log(
+            "Received generatedMustDoList from database: ",
+            docSnapshot.data().generatedMustDoList
+          );
+          setGeneratedMustDoList(docSnapshot.data().generatedMustDoList);
+          setBookmarkList(docSnapshot.data().bookmarkList);
         }
       });
 
       return () => unsubscribe();
-
     } else {
       // if no user is logged in, set generatedMustDoList to the passed in data if any
       if (route.params?.generatedMustDoList) {
-        console.log("Received generatedMustDoList from local: ", route.params.generatedMustDoList)
+        console.log(
+          "Received generatedMustDoList from local: ",
+          route.params.generatedMustDoList
+        );
         setGeneratedMustDoList(route.params.generatedMustDoList);
       }
     }
   }, [isLoggedIn, auth.currentUser]); // Empty dependency array means this effect runs once after the component mounts
-  
+
   const passModalVisible = (visible) => {
-    setModalVisible(visible)
-  }
+    setModalVisible(visible);
+  };
 
   const createReminderHandler = (item) => {
-    setReminderDateTime(new Date()) // default reminder's date and time is now
-    const info = EntryButtonTextHelper(item)
-    const data = {navigation: item};
-    console.log("reminder data: ", data)
-    setReminderInfo({title: info, body: "Tap to know how to " + info, data: data})
-    setModalVisible(true)
-  }
+    setReminderDateTime(new Date()); // default reminder's date and time is now
+    const info = EntryButtonTextHelper(item);
+    const data = { navigation: item };
+    console.log("reminder data: ", data);
+    setReminderInfo({
+      title: info,
+      body: "Tap to know how to " + info,
+      data: data,
+    });
+    setModalVisible(true);
+  };
 
   const passChangedDateTime = (dateTime) => {
-    setReminderDateTime(dateTime)
-  }
+    setReminderDateTime(dateTime);
+  };
 
-  const finishDeletionAlert = () => (
+  const finishDeletionAlert = () =>
     Alert.alert(
       "Data Cleared",
       "Your selections has been successfully cleared.",
       [
         {
           text: "Home",
-          onPress: () => navigation.replace("Home", {
-            screen: "Explore",
-            params: null
-          }),
+          onPress: () =>
+            navigation.replace("Home", {
+              screen: "Explore",
+              params: null,
+            }),
         },
       ]
-    )
-  )
+    );
 
   const clearData = async () => {
     try {
@@ -101,48 +123,52 @@ export default function MustDoList({ navigation, route }) {
       console.error("Error clearing data: ", err);
       Alert.alert("Error", "Failed to clear data.");
     }
-  }
+  };
 
   const handleClearSelections = () => {
-      if(isLoggedIn){
-        Alert.alert(
-          "Notice",
-          "Confirm to clear? This will also wipe out your current Must-Do List.",
-          [
-            {
-              text: "Cancel"
-            },
-            {
-              text: "Confirm",
-              onPress: clearData
-            }]
-        )
-      } else {
-        finishDeletionAlert()
-      }
+    if (isLoggedIn) {
+      Alert.alert(
+        "Notice",
+        "Confirm to clear? This will also wipe out your current Must-Do List.",
+        [
+          {
+            text: "Cancel",
+          },
+          {
+            text: "Confirm",
+            onPress: clearData,
+          },
+        ]
+      );
+    } else {
+      finishDeletionAlert();
+    }
   };
 
   const handleChangeAnswers = () => {
-    if(isLoggedIn){
+    if (isLoggedIn) {
       navigation.navigate("MustDo");
     } else {
-      if(route.params?.userSelection){
-        navigation.navigate("MustDo", {userSelection: route.params.userSelection});
+      if (route.params?.userSelection) {
+        navigation.navigate("MustDo", {
+          userSelection: route.params.userSelection,
+        });
+      }
     }
   };
-}
 
   const handleExplore = () => {
-    if(isLoggedIn){
+    if (isLoggedIn) {
       navigation.navigate("Home");
     } else {
-        navigation.navigate("Home", {
-          screen: "Explore", 
-          params: {
-            generatedMustDoList : generatedMustDoList, 
-            userSelection: route.params?.userSelection
-          }})
-      }
+      navigation.navigate("Home", {
+        screen: "Explore",
+        params: {
+          generatedMustDoList: generatedMustDoList,
+          userSelection: route.params?.userSelection,
+        },
+      });
+    }
   };
 
   const getPhotosFromApi = async () => {
@@ -186,128 +212,129 @@ export default function MustDoList({ navigation, route }) {
   };
 
   return (
-    <View>
+    <ImageBackground
+      source={{ uri: randomImageUrl || null }}
+      style={mustDoListStyles.fullscreen}
+      resizeMode="cover"
+    >
       {/*Modal for setting reminder by dateTime*/}
-        <Modal
-          transparent={true}
-          animationType="fade"
-          visible={modalVisible}
-        >
-          <ReminderSetter
-            reminderInfo={reminderInfo}
-            dateTime={reminderDateTime}
-            changeDateTime={passChangedDateTime}
-            keepOpen={passModalVisible} />
-        </Modal>
-
-      {/*Screen*/}
-      {randomImageUrl ? (
-        <Image
-          source={{ uri: randomImageUrl }}
-          style={{ width: "100%", height: "50%" }}
-          resizeMode="cover"
+      <Modal transparent={true} animationType="fade" visible={modalVisible}>
+        <ReminderSetter
+          reminderInfo={reminderInfo}
+          dateTime={reminderDateTime}
+          changeDateTime={passChangedDateTime}
+          keepOpen={passModalVisible}
         />
-      ) : null}
-
+      </Modal>
       <FlatList
         data={generatedMustDoList}
         renderItem={({ item, index }) => {
           if (item === "nothing") {
             return (
-              <Text>
-                {"Great news!\nYou've covered all the essential tasks based on the information provided."}
-              </Text>);
+              <View style={mustDoListStyles.taskContainer}>
+                <Text style={mustDoListStyles.taskText}>
+                  {
+                    "Great news!\nYou've covered all the essential tasks based on the information provided."
+                  }
+                </Text>
+              </View>
+            );
           } else {
             return (
-            <View style={mustDoListStyles.toDoTask}>
-              <TextButton onPress={() => goToDetails(item)}>
-                <Text>
-                  {EntryButtonTextHelper(item)}
-                </Text>
-              </TextButton>
-              <IconButton
-                  onPress={() => createReminderHandler(item)}
-                  type="reminder"
-                />
-            </View>)
+              <View style={mustDoListStyles.taskContainer}>
+                <View style={mustDoListStyles.toDoTask}>
+                  <TouchableOpacity
+                    onPress={() => goToDetails(item)}
+                    style={mustDoListStyles.buttonTextContainer}
+                  >
+                    <Text style={mustDoListStyles.taskText}>
+                      {EntryButtonTextHelper(item)}
+                    </Text>
+                  </TouchableOpacity>
+                  <IconButton
+                    onPress={() => createReminderHandler(item)}
+                    type="reminder"
+                  />
+                </View>
+              </View>
+            );
           }
         }}
-        />
+      />
 
-      <TextButton onPress={handleClearSelections}>
-        <Text style={mustDoListStyles.clearDataButtonText}>Clear All My Selections</Text>
-      </TextButton>
-      <TextButton onPress={handleChangeAnswers}>
-        <Text style={mustDoListStyles.clearDataButtonText}>Update My Answers</Text>
-      </TextButton>
-      <TextButton onPress={handleExplore}>
-        <Text style={mustDoListStyles.clearDataButtonText}>Ready To Explore</Text>
-      </TextButton>
-    </View>
+      <TouchableOpacity
+        onPress={handleClearSelections}
+        style={mustDoListStyles.button}
+      >
+        <Text style={mustDoListStyles.clearDataButtonText}>
+          Clear All My Selections
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={handleChangeAnswers}
+        style={mustDoListStyles.button}
+      >
+        <Text style={mustDoListStyles.clearDataButtonText}>
+          Update My Answers
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleExplore} style={mustDoListStyles.button}>
+        <Text style={mustDoListStyles.clearDataButtonText}>
+          Ready To Explore
+        </Text>
+      </TouchableOpacity>
+    </ImageBackground>
   );
 }
 
 const mustDoListStyles = StyleSheet.create({
-  clearDataButtonText: {
-    color: "white", // Text color, change as needed
-    fontSize: 16,
+  outerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+  },
+  fullscreen: {
+    flex: 1,
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject, // This ensures the overlay covers the full screen
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
   },
   toDoTask: {
     flexDirection: "row",
-    justifyContent: "space-around",
+    justifyContent: "space-between",
     alignItems: "center",
-  }
-  // clearDataButton: {
-  //   backgroundColor: "#f44336", // Example red color, change as needed
-  //   padding: 10,
-  //   borderRadius: 5,
-  //   alignItems: "center",
-  //   justifyContent: "center",
-  //   margin: 10,
-  // },
-  // clearDataButtonPressed: {
-  //   backgroundColor: "#d32f2f", // Darker shade for pressed state
-  // },
+    borderRadius: 20,
+    marginHorizontal: 10,
+  },
+  taskText: {
+    color: "black",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  taskContainer: {
+    backgroundColor: "rgba(255, 255, 255, 0.9)",
+    borderRadius: 15,
+    marginTop: 5,
+    padding: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  button: {
+    backgroundColor: "#FFB703",
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 20,
+    marginBottom: 10,
+  },
+  clearDataButtonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
 });
-
-      // {!isLoggedIn && <Text>Please log in to view your selections!</Text>}
-
-      // {/* Conditional Button: Get a private health care insurance */}
-      // {["Planning to move", "Just arrived - 3 months"].includes(
-      //   userSelections.lengthInCanada
-      // ) && (
-      //   <TextButton onPress={goToDetails}>
-      //     <Text>Get a private health care insurance</Text>
-      //   </TextButton>
-      // )}
-      // {/* Button: Apply for Medical Service Plan */}
-      // {[
-      //   "Planning to move",
-      //   "Just arrived - 3 months",
-      //   "More than 3 months",
-      // ].includes(userSelections.lengthInCanada) && (
-      //   <TextButton onPress={goToDetails}>
-      //     <Text>Apply for Medical Service Plan</Text>
-      //   </TextButton>
-      // )}
-      // {/* Conditional Button: Apply for Social Insurance Number */}
-      // {userSelections.occupation === "Worker" ||
-      //   (userSelections.studentWork === "Yes" && (
-      //     <TextButton onPress={goToDetails}>
-      //       <Text>Apply for Social Insurance Number</Text>
-      //     </TextButton>
-      //   ))}
-
-      // {/* Conditional Button: Apply for driver's license */}
-      // {userSelections.drive === "Yes" && (
-      //   <TextButton onPress={goToDetails}>
-      //     <Text>Apply for driver's license</Text>
-      //   </TextButton>
-      // )}
-
-      // {/* Conditional Button: Get a compass card */}
-      // {userSelections.needPublicTransportation === "Yes" && (
-      //   <TextButton onPress={goToDetails}>
-      //     <Text>Get a compass card</Text>
-      //   </TextButton>
-      // )}
